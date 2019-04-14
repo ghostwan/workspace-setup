138e2e66d98489c44a4776fe72b510d7580d22bd
Ghostwan <ghostwan@gmail.com>
2019-04-14 23:37:14 +0200

    Add software configuration in install_new_workspace script Ask where to clone the workspace

--- install_new_workspace.sh
+++ install_new_workspace.sh
@@ -1,32 +1,37 @@
-#!/bin/bash
+#!/bin/sh
 
-OPTIND=1 # Reset in case getopts has been used previously in the shell.
+function print() {
+    printf "\033[0;33m $1 \033[0m"
+}
+function println() {
+    printf "\033[0;33m $1 \033[0m\n"
+}
 
-full=false
-
-while getopts "h?f" opt; do
-    case "$opt" in
-    h | \?)
-        echo "Usage: install_new_workspace.sh [-h] [-f]
-Options:
--h Help: Show this screen 
--f Full: install all apps">&2
-        exit 0
-        ;;
-    f)
-        full=true
-        ;;
+function ask_yes_or_no() {
+    read -p "$1 ([y]es or [N]o): "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) answer=true ;;
+        *)     answer=false ;;
     esac
-done
+}
 
-if [ $full = true ]; then
+ask_yes_or_no "Do you want to install the full stack ?"
+full_stack=$answer
+
+if [ $full_stack = true ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ghostwan/workspace-setup/master/install_software_stack.sh)"    
 else
     curl https://raw.githubusercontent.com/ghostwan/workspace-setup/master/install_software_stack.sh | bash -s -- -f -c terminal
 fi
 
-echo "Cloning workspace..."
-# Replace this by your own private workspace, this is my private gws config
+read -p "Where do you want to clone the workspace ? (current directory): "
+path=$REPLY
+if [ -n "$path" ]; then
+    println "Going to $path"
+    eval cd $path
+fi
+println "Cloning workspace..."
+# Replace this repo by your own private workspace, this is my private gws config
 # This repo has a file .projects.gws that describe workspace tree
 # Each branch is a different configuration, master is the core one then I have: personal, work, doc
 # To know more about gws: https://github.com/StreakyCobra/gws 
@@ -34,13 +39,27 @@ git clone https://ghostwan@github.com/ghostwan/workspace
 cd workspace
 rm -f .cache.gws*
 branches=$(git branch -r)
-echo "What configuration do you want ? " 
-echo "${branches//origin\//}"
-read -p "> " branch
+echo "What configuration do you want ?:" 
+branches="${branches/master/master\n}"
+println "${branches//origin\//}"
+read -p ": " branch
 git checkout $branch
 gws update
 # Replace this part by your configuration
 # Finalize my workspace installation by configuring my apps as zsh, alfred, iterm ...
 # Import paid app licence and create symlink
-.bootstrap/configs/install_software_configuration.sh
+ask_yes_or_no "Do you want to configure the software ?"
+if [ $answer = true ]; then 
+    # I cloned a repo named configs where I put all my app configs
+    config_directory=$(pwd)"/configs"
 
+    println "install oh my zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    rm $HOME/.zshrc
+
+    println "Create symlink for zshrc: $config_directory/zsh/zshrc_current"
+    ln -s $config_directory/zsh/zshrc_current $HOME/.zshrc
+
+    println "Add plugin for oh-my-zsh"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
